64221522ff6c6d456176b09b48dc0188b636d7e7
Gabriele Cirulli <gabrycir@gmail.com>
2015-04-18 19:33:48 +0200

    Add templating with HAML

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
-var bower   = require("gulp-bower");
-var gulp    = require("gulp");
-var header  = require("gulp-header");
-var minify  = require("gulp-minify-css");
-var notify  = require("gulp-notify");
-var plumber = require("gulp-plumber");
-var sass    = require("gulp-sass");
+var fs    = require("fs");
+var path  = require("path");
+var spawn = require("child_process").spawn;
+
+var bower      = require("gulp-bower");
+var del        = require("del");
+var ghaml      = require("gulp-haml");
+var gulp       = require("gulp");
+var haml       = require("hamljs");
+var header     = require("gulp-header");
+var livereload = require("gulp-livereload");
+var mapStream  = require("map-stream");
+var minify     = require("gulp-minify-css");
+var notify     = require("gulp-notify");
+var plumber    = require("gulp-plumber");
+var sass       = require("gulp-sass");
 
 var CONFIG = {
+  PATHS: {
+    PUBLIC: "public",
+    SERVER: "node_modules/.bin/http-server",
+  },
+  LAYOUTS: {
+    ORIGIN:  "layouts",
+    DEFAULT: "application",
+  },
+  VIEWS: {
+    ORIGIN: "views",
+    DEST:   "public",
+  },
   STYLESHEETS: {
-    ORIGIN: "./assets/stylesheets",
-    DEST:   "./public/stylesheets",
+    ORIGIN: "assets/stylesheets",
+    DEST:   "public/stylesheets",
   },
   JAVASCRIPTS: {
-    ORIGIN: "./assets/javascripts",
-    DEST:   "./public/javascripts",
+    ORIGIN: "assets/javascripts",
+    DEST:   "public/javascripts",
   },
   BOWER: {
-    DEST: "./bower_components",
+    DEST: "bower_components",
   },
 };
 
@@ -25,7 +46,7 @@ var CONFIG = {
 var notice = function (location) {
   return header([
     "/* Don't edit this file directly. Make changes in",
-    location.replace("./", ""),
+    location,
     "instead. */\n",
   ].join(" "));
 };
@@ -46,6 +67,27 @@ gulp.task("bower", function () {
     .pipe(gulp.dest(CONFIG.BOWER.DEST));
 });
 
+gulp.task("haml", function () {
+  var layoutPath = path.join(
+    CONFIG.LAYOUTS.ORIGIN, CONFIG.LAYOUTS.DEFAULT + ".haml"
+  );
+
+  var layout = fs.readFileSync(layoutPath);
+
+  gulp.src(CONFIG.VIEWS.ORIGIN + "/**/*.haml")
+      .pipe(errorHandler())
+      .pipe(ghaml({ compiler: "visionmedia" }))
+      .pipe(mapStream(function (file, callback) {
+        file.contents = new Buffer(haml.render(layout, {
+          locals: { content: file.contents }
+        }));
+
+        callback(null, file);
+      }))
+      .pipe(gulp.dest(CONFIG.VIEWS.DEST))
+      .pipe(livereload());
+});
+
 gulp.task("stylesheets", function () {
   gulp.src(CONFIG.STYLESHEETS.ORIGIN + "/application.scss")
       .pipe(errorHandler())
@@ -56,19 +98,41 @@ gulp.task("stylesheets", function () {
       }))
       .pipe(minify())
       .pipe(notice(CONFIG.STYLESHEETS.ORIGIN))
-      .pipe(gulp.dest(CONFIG.STYLESHEETS.DEST));
+      .pipe(gulp.dest(CONFIG.STYLESHEETS.DEST))
+      .pipe(livereload());
 });
 
 gulp.task("javascripts", function () {
   gulp.src(CONFIG.JAVASCRIPTS.ORIGIN  + "/index.js")
       .pipe(errorHandler())
       .pipe(notice(CONFIG.JAVASCRIPTS.ORIGIN))
-      .pipe(gulp.dest(CONFIG.JAVASCRIPTS.DEST));
+      .pipe(gulp.dest(CONFIG.JAVASCRIPTS.DEST))
+      .pipe(livereload());
 });
 
-gulp.task("default", ["bower", "stylesheets", "javascripts"]);
+gulp.task("default", ["bower", "haml", "stylesheets", "javascripts"]);
 
 gulp.task("watch", ["default"], function () {
+  livereload.listen();
+
+  gulp.watch(CONFIG.VIEWS.ORIGIN + "/**/*.haml",       ["haml"]);
+  gulp.watch(CONFIG.LAYOUTS.ORIGIN + "/**/*.haml",     ["haml"]);
   gulp.watch(CONFIG.STYLESHEETS.ORIGIN + "/**/*.scss", ["stylesheets"]);
   gulp.watch(CONFIG.JAVASCRIPTS.ORIGIN + "/**/*.js",   ["javascripts"]);
+});
+
+gulp.task("dev", ["watch"], function () {
+  var proc = spawn(CONFIG.PATHS.SERVER, [CONFIG.PATHS.PUBLIC, "-p4000"]);
+
+  /* eslint-disable no-console */
+  proc.stdout.on("data", function (data) { console.log(data.toString()); });
+  /* eslint-enable */
+});
+
+gulp.task("clean", function () {
+  del([CONFIG.PATHS.PUBLIC], function (err, deletedFiles) {
+    /* eslint-disable no-console */
+    console.log("Deleted:", deletedFiles.join(", "));
+    /* eslint-enable */
+  });
 });
